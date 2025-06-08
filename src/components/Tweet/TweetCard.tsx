4453d6b7de6bfbc21a0d60d1a1bdac46b17834ef
Archie Facundo Junio <166824471+QuantaFX@users.noreply.github.com>
2025-06-09 03:49:33 +0800

    Updated TweetCard.tsx

--- src/components/Tweet/TweetCard.tsx
+++ src/components/Tweet/TweetCard.tsx
@@ -54,7 +54,7 @@ export const TweetCard: React.FC<TweetCardProps> = ({
 
   return (
     <div className="border-b border-gray-200 p-4 hover:bg-gray-50 transition-colors">
-      <div className="flex flex-row-reverse gap-5">
+      <div className="flex flex-row-reverse gap-3">
         {/* Avatar - Now on the right */}
         <Avatar className="w-12 h-12 flex-shrink-0">
           <AvatarImage src={tweet.author.avatar} />
@@ -62,7 +62,7 @@ export const TweetCard: React.FC<TweetCardProps> = ({
         </Avatar>
 
         {/* Tweet Content - Now on the left but text-aligned right */}
-        <div className="flex-1 min-w-0 text-right mr-3">
+        <div className="flex-1 min-w-0 text-right mr-1">
           {/* Header */}
           <div className="flex items-center justify-between mb-1">
             {/* More Options - Now on the left */}
