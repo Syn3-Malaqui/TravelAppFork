92a806617a281090d3f617f944e7fc2f9ca0659f
Archie Facundo Junio <166824471+QuantaFX@users.noreply.github.com>
2025-06-09 03:36:12 +0800

    Updated MobileTweetCard.tsx

--- src/components/Tweet/MobileTweetCard.tsx
+++ src/components/Tweet/MobileTweetCard.tsx
@@ -52,7 +52,7 @@ export const MobileTweetCard: React.FC<MobileTweetCardProps> = ({
 
   return (
     <div className="border-b border-gray-100 p-4 bg-white">
-      <div className="flex space-x-6 flex-row-reverse">
+      <div className="flex space-x-10 flex-row-reverse">
         {/* Avatar - Now on the right */}
         <Avatar className="w-10 h-10 flex-shrink-0">
           <AvatarImage src={tweet.author.avatar} />
